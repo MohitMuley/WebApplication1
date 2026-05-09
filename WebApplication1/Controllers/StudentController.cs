564209e3e109457f179913f6de16e8fafea36743
MohitMuley <muleymohit199@gmail.com>
2026-05-10 02:57:20 +0530

    Add project files.

---
+++ WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+
+        public List<Student> GetStudents() 
+        {
+            return new List<Student>{
+                new Student
+                {
+                    id = 1,
+                    name = "mohit",
+                    email = "xyz@gmail.com",
+                    address = "pune maharashtra"
+
+                },
+                new Student
+                {
+                    id = 2,
+                    name = "xyz",
+                    email = "mm@gmail.com",
+                    address = "maharashtra"
+                }
+
+            }; 
+            
+        }
+    }
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int id {  get; set; }
+        public required string name { get; set; }
+        public required string email { get; set; }
+        public required string address { get; set; }
+
+    }
+}
